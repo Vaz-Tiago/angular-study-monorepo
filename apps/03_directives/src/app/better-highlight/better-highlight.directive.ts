d5038834488e12b21728db1242548b497d4043cf
Tiago Vaz <tiago.vaz@hotmail.com>
2023-03-28 20:35:06 -0300

    feat(directives): PAssing dynamic props

--- apps/03_directives/src/app/better-highlight/better-highlight.directive.ts
+++ apps/03_directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,18 +1,54 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'darkblue';
+  @Input('appBetterHighlight') highlightColor: string = 'purple';
+  // @Input() highlightColor: string = 'purple';
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  @HostBinding('style.padding') padding = '10px';
+  @HostBinding('style.color') color = 'white';
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'backgroundColor',
-      'darkblue'
-    );
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
-    this.renderer.setStyle(this.elRef.nativeElement, 'padding', '10px');
+    this.backgroundColor = this.defaultColor;
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'backgroundColor',
+    //   'darkblue'
+    // );
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'padding', '10px');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.backgroundColor = this.highlightColor;
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'backgroundColor',
+    //   'purple'
+    // );
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.backgroundColor = this.defaultColor;
+
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'backgroundColor',
+    //   'darkblue'
+    // );
   }
 }
