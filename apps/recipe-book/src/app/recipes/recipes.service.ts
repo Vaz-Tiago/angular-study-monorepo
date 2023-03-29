8122c2aeb9e667acae6142b377c7958fad77f7c3
Tiago Vaz <tiago.vaz@hotmail.com>
2023-03-29 18:39:22 -0300

    feature(recipe-book): add ingredients to recipe

--- apps/recipe-book/src/app/recipes/recipes.service.ts
+++ apps/recipe-book/src/app/recipes/recipes.service.ts
@@ -1,18 +1,29 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipes.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'This is a simple test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true'
+      'X-Burger',
+      'Esse é burgão da massa',
+      'https://cdn.pigz.app/pigzapp/product/01FMTG35FMC7XB1GX6WGPH01BM.webp',
+      [
+        new Ingredient('Hamburger', 1),
+        new Ingredient('Queijo', 2),
+        new Ingredient('Batatas fritas', 20),
+      ]
     ),
     new Recipe(
-      'A second test recipe',
-      'This is a simple second test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true'
+      'Cachorro Quente',
+      'Prensado sucesso',
+      'https://kahdog.com.br/wp-content/uploads/2021/07/Fotos-Site-Kahdog20.jpg',
+      [
+        new Ingredient('Salsicha', 1),
+        new Ingredient('Pão', 1),
+        new Ingredient('Batata palha', 50),
+      ]
     ),
   ];
 
