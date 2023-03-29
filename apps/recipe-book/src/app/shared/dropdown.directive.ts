439ef30cc61a7810cf95c306c4094fa294081e6f
Tiago Vaz <tiago.vaz@hotmail.com>
2023-03-28 21:35:35 -0300

    refactor(recipe-book): simplify with HostBinding and add dropdown on header

--- apps/recipe-book/src/app/shared/dropdown.directive.ts
+++ apps/recipe-book/src/app/shared/dropdown.directive.ts
@@ -1,28 +1,12 @@
-import {
-  Directive,
-  ElementRef,
-  HostListener,
-  OnInit,
-  Renderer2,
-} from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
-export class DropdownDirective implements OnInit {
-  isOpen = false;
-  initialClass: string;
-
-  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
-  ngOnInit(): void {
-    this.initialClass = this.elRef.nativeElement.className;
-  }
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
 
   @HostListener('click') toggleOpen(eventData: Event) {
     this.isOpen = !this.isOpen;
-    const className = this.isOpen
-      ? `${this.initialClass} open`
-      : this.initialClass;
-    this.renderer.setAttribute(this.elRef.nativeElement, 'class', className);
   }
 }
