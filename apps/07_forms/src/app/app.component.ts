c8f678d2c36e8a2afe4fe6a6b8dddeaaf5ccf59c
Tiago Vaz <tiago.vaz@hotmail.com>
2023-04-01 00:37:26 -0300

    feat(forms): add submit and reset actions

--- apps/07_forms/src/app/app.component.ts
+++ apps/07_forms/src/app/app.component.ts
@@ -11,18 +11,27 @@ export class AppComponent {
   answer: string;
   defaultSecretValue = 'pet';
   gender = ['male', 'female'];
+  submitted = false;
+
+  user = {
+    username: '',
+    mail: '',
+    question: '',
+    answer: '',
+    gender: '',
+  };
   suggestUserName() {
     const suggestedName = 'Superuser';
     // replace all the form
-    // this.userForm.setValue({
-    //   userData: { username: 'teste', email: 'teste@teste.com' },
-    //   secret: 'teacher',
-    //   questionAnswer: 'test',
-    //   gender: 'male',
-    // });
+    this.userForm.setValue({
+      userData: { username: 'teste', email: 'teste@teste.com' },
+      secret: 'teacher',
+      questionAnswer: 'test',
+      gender: 'male',
+    });
 
     // do not replace all
-    this.userForm.form.patchValue({ userData: { username: suggestedName } });
+    // this.userForm.form.patchValue({ userData: { username: suggestedName } });
   }
 
   // onSubmit(form: NgForm) {
@@ -30,6 +39,15 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.userForm);
+    this.user.username = this.userForm.value.userData.username;
+    this.user.mail = this.userForm.value.userData.email;
+    this.user.question = this.userForm.value.secret;
+    this.user.answer = this.userForm.value.questionAnswer;
+    this.user.gender = this.userForm.value.gender;
+
+    // in that case, we can pass an object to reset, to reset with default values
+    this.userForm.resetForm();
+
+    this.submitted = true;
   }
 }
