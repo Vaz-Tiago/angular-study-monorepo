1d23bc58c8513a75053517b65b7acabe0ab8fd61
Tiago Vaz <tiago.vaz@hotmail.com>
2023-03-31 23:44:13 -0300

    feat(forms): add form communication

--- apps/07_forms/src/app/app.component.ts
+++ apps/07_forms/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f', { static: false }) form: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.debug(form);
+  // }
+
+  onSubmit() {
+    console.log(this.form);
   }
 }
