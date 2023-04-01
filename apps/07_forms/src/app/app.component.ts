0a1736943e1cf18c84a463623447fbfe8a517396
Tiago Vaz <tiago.vaz@hotmail.com>
2023-04-01 00:00:25 -0300

    feat(forms): default value

--- apps/07_forms/src/app/app.component.ts
+++ apps/07_forms/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', { static: false }) userForm: NgForm;
+  defaultSecretValue = 'pet';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
